97da4424eacb9c0f63549f07c602b2a6346f0f40
CezarFelipe <cezarfelipedasilva@gmail.com>
2021-11-13 13:32:29 -0300

    ribbon load balancing

--- hr-payroll/src/main/java/br/edu/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/br/edu/hrpayroll/feignclients/WorkerFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Component
-@FeignClient(name ="hr-worker", url="localhost:8001", path ="/workers")
+@FeignClient(name ="hr-worker", path ="/workers")
 public interface WorkerFeignClient {
 
 	@GetMapping(value="/{id}")
--- hr-worker/src/main/java/br/edu/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/edu/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package br.edu.hrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,12 @@ import br.edu.hrworker.repository.WorkerRepository;
 @RequestMapping(value ="/workers")
 public class WorkerResource {
 
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerRepository.class);
+	
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	private WorkerRepository repository;
 	
@@ -27,6 +36,9 @@ public class WorkerResource {
 	
 	@GetMapping(value="/{id}")
 	public ResponseEntity<Worker>findId (@PathVariable Long id){
+		
+		logger.info("PORT ===" + env.getProperty("local.server.port"));
+		
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}
