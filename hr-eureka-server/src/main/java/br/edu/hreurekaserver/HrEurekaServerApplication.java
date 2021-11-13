6e5d48463edca141824ac683ad5d1e5d9c92e8ac
CezarFelipe <cezarfelipedasilva@gmail.com>
2021-11-13 13:39:13 -0300

    Project Eureka hr server created

---
+++ hr-eureka-server/src/main/java/br/edu/hreurekaserver/HrEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package br.edu.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaServerApplication.class, args);
+	}
+
+}
