20e7f6e5d2286332dec5a834d37af1e1ec062509
CezarFelipe <cezarfelipedasilva@gmail.com>
2021-11-12 23:52:10 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/br/edu/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
