6864d7ca0f3feee8475fc7d701d956d212e1377d
CezarFelipe <cezarfelipedasilva@gmail.com>
2021-11-20 12:50:00 -0300

    Project hr-zuul gateway

---
+++ hr-api-gateway-zuul/src/main/java/br/edu/hrpigatewayzuul/HrApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package br.edu.hrpigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ hr-api-gateway-zuul/src/test/java/br/edu/hrpigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.hrpigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
