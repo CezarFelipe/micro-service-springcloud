53d2549ea3973862d6f57081bf915a0564fe077c
CezarFelipe <cezarfelipedasilva@gmail.com>
2021-11-20 23:31:47 -0300

    Project hr-oath created

---
+++ hr-oauth/src/test/java/br/edu/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
