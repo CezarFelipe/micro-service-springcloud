2ed8e53961805234b94984ec66a001f276453e10
CezarFelipe <cezarfelipedasilva@gmail.com>
2021-11-27 19:49:18 -0300

    hr-user Postgres config

---
+++ hr-user/create.sql
@@ -0,0 +1,6 @@
+create table tb_role (id  bigserial not null, role_name varchar(255), primary key (id));
+create table tb_user (id  bigserial not null, email varchar(255), name varchar(255), password varchar(255), primary key (id));
+create table tb_user_role (user_id int8 not null, role_id int8 not null, primary key (user_id, role_id));
+alter table tb_user add constraint UK_4vih17mube9j7cqyjlfbcrk4m unique (email);
+alter table tb_user_role add constraint FKea2ootw6b6bb0xt3ptl28bymv foreign key (role_id) references tb_role;
+alter table tb_user_role add constraint FK7vn3h53d0tqdimm8cp45gc0kl foreign key (user_id) references tb_user;
